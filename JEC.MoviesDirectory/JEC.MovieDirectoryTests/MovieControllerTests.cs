70f1490a129c923f002803d060166e08756d1eed
joie-isikhueme <joie.isikhueme@gmail.com>
2023-07-17 08:45:14 +0100

    added moviecontroller tests

---
+++ JEC.MoviesDirectory/JEC.MovieDirectoryTests/MovieControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using JEC.MoviesDirectory.MoviesController;
+
+namespace JEC.MovieDirectoryTests
+{
+    [TestClass]
+    public class MovieControllerTests
+    {
+        [TestMethod]
+        public void MovieGetSuccess()
+        {
+            var controller = new MoviesController();
+
+            var response = controller.Get("Titanic",1)
+            var contentResult = response asOkNegotiatedContentResult<SearchResult>;
+
+            Assert.IsNotNull(contentResult);
+            Assert.IsNotNulle(contentResult.Content);
+            Asset.AreEqual("Titanic",1, contentResult.Content.SearchResult);
+        }
+
+
+        [TestMethod]
+        public void MovieGetByIdSuccess()
+        {
+            var controller = new MoviesController();
+
+            var response = controller.Get("tt3896198")
+            var contentResult = response asOkNegotiatedContentResult<Movie>;
+
+            Assert.IsNotNull(contentResult);
+            Assert.IsNotNulle(contentResult.Content);
+            Asset.AreEqual("tt3896198", contentResult.Content.Movie);
+        }
+    }
+}
---
+++ JEC.MoviesDirectory/JEC.MovieDirectoryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JEC.MovieDirectoryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JEC.MovieDirectoryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f127e72e-3211-4de9-afa0-95df53e3a33e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
